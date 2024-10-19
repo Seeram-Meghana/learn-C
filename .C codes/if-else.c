71adab23a03d060634375bf21a0ee85345f0733e
Seeram-Meghana <seermeghan7@gmail.com>
2024-10-19 21:13:48 +0530

    checkin

--- .C codes/if-else.c
+++ .C codes/if-else.c
@@ -4,12 +4,16 @@ int main()
     int age;
     printf("enter your age:");
     scanf("%d", &age);
-    if (age > 18) {
+    if (age >= 18) {
         printf("you are adult\n");
-        printf("you can drive");
     }
-    else {
-        printf("you are not an adult");
+    else if (age > 13 && age < 18 )
+    {
+        printf("teenager");
+    }
+    else 
+    {
+        printf("child");
     }
     return 0;
 }
