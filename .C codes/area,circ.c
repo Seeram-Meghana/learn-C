840a09684f2e94d9758d4106b0795eb00c1a9886
Seeram-Meghana <seermeghan7@gmail.com>
2024-11-05 21:02:26 +0530

    chekin

---
+++ .C codes/area,circ.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+# define pi 3.1416
+int r=6;
+int main()
+{
+float area,circ;
+area=pi*r*r;
+circ=2*pi*r;
+printf("area%f",area);
+printf("circumference%f",circ);
+    return 0;
+}
