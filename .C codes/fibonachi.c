c3a816a36e9fc87b01a15b7f16e59a94e34ffd14
Seeram-Meghana <seermeghan7@gmail.com>
2024-11-06 22:29:15 +0530

    checkin

---
+++ .C codes/fibonachi.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int i,n,c,a=0,b=1;
+    scanf("%d", &n);
+    for(i=0;i<n;i++)
+        if(i<=1)
+        {
+            c=i;
+            printf("%d", c);
+        }
+        else {
+            c=a+b;
+            a=b;
+            b=c;
+            printf("%d", c);
+        }
+    return 0;
+}
