bdd18cc97d24b858abe2c0b8a396428aef0e81b7
Seeram-Meghana <seermeghan7@gmail.com>
2024-11-06 22:31:32 +0530

    checkin

--- .C codes/fibonachi.c
+++ .C codes/fibonachi.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 int main()
 {
-    int i,n,c,a=0,b=1;
+    int i, n, c, a = 0, b = 1;
     scanf("%d", &n);
-    for(i=0;i<n;i++)
-        if(i<=1)
+    for (i = 0; i < n; i++)
+        if (i <= 1)
         {
-            c=i;
+            c = i;
             printf("%d", c);
         }
-        else {
-            c=a+b;
-            a=b;
-            b=c;
+        else
+        {
+            c = a + b;
+            a = b;
+            b = c;
+
             printf("%d", c);
         }
     return 0;
